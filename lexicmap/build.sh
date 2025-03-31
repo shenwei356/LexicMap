d508ad1b894fa7ca84db7aee217a9e3ed2b42110
Wei Shen <shenwei356@gmail.com>
2025-03-31 16:17:46 +0800

    fix build.sh

--- lexicmap/build.sh
+++ lexicmap/build.sh
@@ -2,6 +2,6 @@
 
 commit=$(git rev-parse --short HEAD)
 
-go build -trimpath -o=lexicmap -ldflags="-s -w -X github.com/shenwei356/LexicMap/lexicmap/cmd.COMMIT=$commit" -tags netgo
+CGO_ENABLED=0 go build -trimpath -o=lexicmap -ldflags="-s -w -X github.com/shenwei356/LexicMap/lexicmap/cmd.COMMIT=$commit" -tags netgo
 
 ./lexicmap version
